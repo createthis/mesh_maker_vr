368a0717353868a8db6d7f30e6e51738e6128fc2
Jesse CreateThis <jesse@createthis.com>
2017-07-08 08:37:03 -0400

    Add a missing script that was throwing warnings.

---
+++ Assets/MMVR/UI/Controller/ShowHideTextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ShowHideTextController : MonoBehaviour {
+    public string prefix;
+    public GameObject textTarget;
+    public string activeTag;
+
+
+    public void Changed() {
+        GameObject activeTagObj = GameObject.FindWithTag(activeTag);
+        bool isTrue = activeTagObj && activeTagObj.activeSelf;
+        textTarget.GetComponent<TextMesh>().text = prefix + ":\n " + ((isTrue) ? "Hide" : "Show");
+    }
+
+	// Use this for initialization
+	void Start () {
+        Changed();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Changed();
+    }
+}
